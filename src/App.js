202b192511347f3fbadae3d7117142fea8fb4a35
Myroslav Sokolov <mirossoks@gmail.com>
2022-02-16 13:09:35 +0200

    add lockalStorage

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
-import React, { Component } from "react";
-import { nanoid } from "nanoid";
-import ContactForm from "./components/ContactForm";
-import Filter from "./components/Filter";
-import ContactList from "./components/ContactList";
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import ContactForm from './components/ContactForm';
+import Filter from './components/Filter';
+import ContactList from './components/ContactList';
 
 class App extends Component {
   state = {
     contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
     ],
-    filter: "",
+    filter: '',
   };
 
-  deleteContact = (event) => {
+  componentDidMount() {
+    this.setState({ contacts: JSON.parse(localStorage.getItem('contacts')) });
+    console.log(localStorage.getItem('contacts'))
+    if (this.state.contacts = []) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  }
+
+  deleteContact = event => {
     event.preventDefault();
     const idDeletedContact = event.currentTarget.id;
-    this.setState((prevState) => ({
+    this.setState(prevState => ({
       contacts: prevState.contacts.filter(
-        (contact) => contact.id !== idDeletedContact
+        contact => contact.id !== idDeletedContact
       ),
     }));
   };
 
-  addContact = (contact) => {
+  addContact = contact => {
     contact = {
       id: nanoid(),
       name: contact.name,
@@ -33,7 +41,7 @@ class App extends Component {
     };
     if (
       !this.state.contacts.find(
-        (oldContact) =>
+        oldContact =>
           oldContact.name.toLowerCase() === contact.name.toLowerCase()
       )
     ) {
@@ -45,14 +53,14 @@ class App extends Component {
     }
   };
 
-  addFilter = (e) => {
+  addFilter = e => {
     this.setState({
       filter: e.target.value,
     });
   };
 
   filterContacts = () =>
-    this.state.contacts.filter((contact) =>
+    this.state.contacts.filter(contact =>
       contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
     );
 
